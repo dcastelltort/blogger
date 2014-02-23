b05cf033ada13d93bb5da9cb9da5bb9e73869174
Damien Castelltort <dcastelltort@gmail.com>
2014-02-23 01:07:02 +0100

    fix equals/hashcode methods on Comment

--- src/main/java/com/shenten/blogger/comment/model/Comment.java
+++ src/main/java/com/shenten/blogger/comment/model/Comment.java
@@ -112,13 +112,35 @@ public final class Comment implements Comparable<Comment> {
 		return dateCreated.compareTo(o.dateCreated);
 	}
 	
-	 @Override
-	 public boolean equals(Object o) { 
-		 if (o == null || o.getClass() != this.getClass()) {
-			 return false;
-		 }
-		 else {
-			 return dateCreated.equals(((Comment)o).dateCreated);
-		 }
+	 /* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((dateCreated == null) ? 0 : dateCreated.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Comment))
+			return false;
+		Comment other = (Comment) obj;
+		if (dateCreated == null) {
+			if (other.dateCreated != null)
+				return false;
+		} else if (!dateCreated.equals(other.dateCreated))
+			return false;
+		return true;
 	}
 }
